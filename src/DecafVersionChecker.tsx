1624ecfa2b02c5c286b9f2c14e5d9c6d8e2f4a48
alioguzhan <aoguzhanyildiz@gmail.com>
2022-06-10 13:02:45 +0300

    chore: add PUBLIC_URL prefix to version checker URL

--- src/DecafVersionChecker.tsx
+++ src/DecafVersionChecker.tsx
@@ -91,7 +91,7 @@ export default function DecafVersionChecker(props: DecafVersionCheckerProps) {
       clearInterval(interval.current);
     }
     interval.current = window.setInterval(() => {
-      fetch('/version.json?t=' + new Date().getTime())
+      fetch(process.env.PUBLIC_URL + '/version.json?t=' + new Date().getTime())
         .then((res) => res.json())
         .then((data) => {
           setNewVersion(data.version);
