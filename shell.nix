426bc92ac3def16e871abbb55c0f8c2a1dfd2566
Ali Oguzhan Yildiz <aoguzhanyildiz@gmail.com>
2023-05-19 10:53:11 +0300

    chore: use current lts for testing and development

--- shell.nix
+++ shell.nix
@@ -1,11 +1,11 @@
-with import (fetchTarball https://github.com/NixOS/nixpkgs/archive/22.05.tar.gz) {};
+with import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/22.11.tar.gz") {};
 
 stdenv.mkDerivation {
   name = "decaf-react";
 
   buildInputs = with pkgs; [
     git
-    nodejs-16_x
+    nodejs # always LTS
     yarn
 
     figlet
